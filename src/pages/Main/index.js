e4e074de3b82b4c69f8e33a6dcbc95b851f70043
Vanessah4money@gamil.com <Vanessah4money@gamil.com>
2021-01-07 09:38:19 -0300

    Acessando Api do GIthub

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { View , Text} from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard,View , Text} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form,Input, SubmitButton } from './styles';
+import api from'../../services/api';
 
-const Main = () => {
-  return (
-  <Container>
-    <Form>
-      <Input autoCorrect={false} autoCapitalize="none" placeholder="Adicionar Usúario"/>
-      <SubmitButton>
-       <Icon name="add" size={20} color="#FFF" />
-     </SubmitButton>
-    </Form>
+export default class Main extends Component {
+  state ={
+    newUser:'',
+    users : [],
+  }
+  handleAddUser =  async () =>{
+    const{users,newUser} = this.state;
+    const response = await api.get(`/users/${newUser}`);
+    const data ={
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    }
+    this.setState({users:[...users,data],newUser:''});
+    Keyboard.dismiss();
+  }
+  render(){
+    const {users,newUser} = this.state;
+    return (
+    <Container>
+      <Form>
+        <Input autoCorrect={false} value={newUser} onChangeText={text => this.setState({newUser: text})} retornKeyType="send" onSubmitEditing={handleAddUser} autoCapitalize="none" placeholder="Adicionar Usúario"/>
+        <SubmitButton onPress={handleAddUser}>
+        <Icon name="add" size={20} color="#FFF" />
+      </SubmitButton>
+      </Form>
 
-  </Container>
-  );
+    </Container>
+    );
+  }
 }
 
-export default Main;
