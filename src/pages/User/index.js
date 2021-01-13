6741799a64796a6c5bd099d86e4208e91f1c0958
Vanessah4money@gamil.com <Vanessah4money@gamil.com>
2021-01-13 09:39:35 -0300

    Buscando dados da Api

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import api from '../../services/api';
+import propTypes from 'prop-types';
 
 // import { Container } from './styles';
 
-const User = ({route}) => {
-  console.tron.log(route.params.user);
-  return <View />;
+export default class User extends Component {
+  state = {
+    stars:[],
+  }
+  static propTypes ={
+    navigation: propTypes.shape({
+      navigate:propTypes.func,
+    }).isRequired,
+  }
+  async componentDidMount(){
+    const{navigation} = this.props;
+    const {user} =  this.props.route.params;
+    const response = await api.get(`/users/${user.login}/starred`);
+    console.tron.log(response);
+    this.setState({stars: response.data});
+
+  }
+  render( ){
+    return <View/>
+  }
 }
 
-export default User;
+
