76756ec93b2721df67c80f89792512792a0e3fca
Vanessah4money@gamil.com <Vanessah4money@gamil.com>
2021-01-13 10:28:29 -0300

    Listando Favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import api from '../../services/api';
 import propTypes from 'prop-types';
 
-// import { Container } from './styles';
+import { Container, Header,Avatar, Name, Bio,Stars,Starred,OwnerAvatar,Info,Title,Author } from './styles';
 
 export default class User extends Component {
   state = {
@@ -15,15 +15,42 @@ export default class User extends Component {
     }).isRequired,
   }
   async componentDidMount(){
-    const{navigation} = this.props;
     const {user} =  this.props.route.params;
     const response = await api.get(`/users/${user.login}/starred`);
     console.tron.log(response);
     this.setState({stars: response.data});
+    console.tron.log(this.state.stars);
 
   }
   render( ){
-    return <View/>
+    const{stars} = this.state;
+    const {user} =  this.props.route.params;
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri:user.avatar}}></Avatar>
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        <Stars
+          data={stars}
+          keyExtractor={ star => String(star.id)}
+          renderItem={({item})=>(
+            <Starred>
+              <OwnerAvatar source={{uri:item.owner.avatar_url}} />
+              <Info>
+                <Title>
+                  {item.name}
+                </Title>
+                <Author>
+                  {item.owner.login}
+                </Author>
+              </Info>
+            </Starred>
+          )}
+         />
+      </Container>
+    );
   }
 }
 
